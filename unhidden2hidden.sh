c51df14dff758dc7d5cf4d7a73df11573a7f5a82
Tarek Allam <t.allam.jr@gmail.com>
2015-12-29 22:31:07 +0000

    Adds description of script within file

--- unhidden2hidden.sh
+++ unhidden2hidden.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Prepends each file listed using the 'ls' command
+# with a dot to render it a hidden file instead of unhidden.
+
         for i in $( ls ); do
             mv $i .$i
         done
